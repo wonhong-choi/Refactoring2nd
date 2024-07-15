f176d715570bc87b275b09c3291a4d3c4bef967e
wonhong <cwh0099@naver.com>
2024-07-15 22:31:07 +0900

    extract function (amount_for)

--- statement.py
+++ statement.py
@@ -22,14 +22,8 @@ class Invoice:
     
 
 def statement(invoice:Invoice, plays:dict) -> str:
-    total_amount = 0
-    volume_credits = 0
-    result = f"invoice (customer : {invoice.customer})\n"
-    
-    for perf in invoice.performances:
-        play = plays[perf.play_id]
+    def amount_for(perf, play):
         this_amount = 0
-        
         match play.type:
             case "tragedy":     # tragedy
                 this_amount = 40000
@@ -42,7 +36,16 @@ def statement(invoice:Invoice, plays:dict) -> str:
                 this_amount += 300 * perf.audience
             case _:
                 raise Exception(f"Not supported genre: {play.type}")
-            
+        return this_amount
+    
+    total_amount = 0
+    volume_credits = 0
+    result = f"invoice (customer : {invoice.customer})\n"
+    
+    for perf in invoice.performances:
+        play = plays[perf.play_id]
+        this_amount = amount_for(perf, play)
+        
         volume_credits += max(perf.audience - 30, 0)
         if play.type == "comedy":
             volume_credits += math.floor(perf.audience / 5)
