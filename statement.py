94124b0d85e80c49a02e4a6a0401d04fa6da56fc
wonhong <cwh0099@naver.com>
2024-07-16 23:09:54 +0900

    html_statement

--- statement.py
+++ statement.py
@@ -3,7 +3,6 @@ import math
 
 from .create_statement_data import *
 
-
 locale.setlocale(locale.LC_ALL, '')
 
 
@@ -23,10 +22,10 @@ class Invoice:
         self.customer = customer
         self.performances = performances
     
+def usd(number):
+    return number // 100
 
 def render_plain_text(data:dict) -> str:
-    def usd(number):
-        return number // 100
     
     result = f"invoice (customer : {data['customer']})\n"
     for perf in data['performances']:
@@ -36,6 +35,21 @@ def render_plain_text(data:dict) -> str:
     result += f"volume credits: {data['total_volume_credits']} points"
     return result
 
+def render_html(data:dict) -> str:
+    result = f"<h1> invoice (customer : {data['customer']})\n"
+    result += "<table>\n"
+    result += "<tr><th>play</th><th>seats</th><th>price</th></tr>"
+    for perf in data['performances']:
+        result += f"<tr><td>{perf['play'].name}</td><td>{perf['audience']} seats</td>"
+        result += f"<td>{usd(perf['amount'])}</td></tr>\n"
+    result += "</table>\n"
+    result += f"<p>total: <em>{usd(data['total_amount'])}</em></p>\n"
+    result += f"<p>volume credits: <em>{data['total_volume_credits']}</em></p>\n"
+    return result
+
 
 def statement(invoice:Invoice, plays:dict) -> str:
     return render_plain_text(create_statement_data(invoice, plays))
+
+def html_statement(invoice:Invoice, plays:dict) -> str:
+    return render_html(create_statement_data(invoice, plays))
